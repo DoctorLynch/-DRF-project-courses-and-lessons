628821835fa83a741182183cfe53c0c3d09f8c18
DoctorLynch <doctor_linchenko@mail.ru>
2023-12-02 02:28:36 +0300

    module 7 HW2

--- course/permissions.py
+++ course/permissions.py
@@ -1,22 +1,33 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsOwner(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user == view.obj.owner
-
-
-class IsStaff(BasePermission):
-    def has_permission(self, request, view):
-        if request.user.is_staff:
-            return True
-        else:
-            return False
-
-
-class IsSuperuser(BasePermission):
+class IsOwnerOrIsSuperuserOrIsStaff(BasePermission):
     def has_permission(self, request, view):
         if request.user.is_superuser:
             return True
-        else:
+        elif request.user.is_staff and (view.action != 'destroy' or view.action != 'create'):
+            return True
+        return request.user == view.get_object().owner and (view.action == 'list' or view.action == 'update')
+
+
+class IsStaffOrOwner(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_staff:
+            return True
+
+        return request.user == view.get_object().owner
+
+
+class IsSuperuserOrIsOwner(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        return request.user == view.get_object().owner
+
+
+class NotStaff(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_staff:
             return False
+        else:
+            return True
--- course/views.py
+++ course/views.py
@@ -4,7 +4,8 @@ from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 
 from course.models import Course, Lesson, Payments
-from course.permissions import IsOwner, IsSuperuser, IsStaff
+from course.permissions import IsOwnerOrIsSuperuserOrIsStaff, IsSuperuserOrIsOwner, NotStaff, \
+    IsStaffOrOwner
 from course.serializers import LessonSerializer, PaymentsSerializer, LessonListSerializer, \
     LessonDetailSerializer, CourseListSerializer
 
@@ -12,42 +13,42 @@ from course.serializers import LessonSerializer, PaymentsSerializer, LessonListS
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseListSerializer
     queryset = Course.objects.all()
-    permission_classes = [IsOwner]
+    permission_classes = [IsOwnerOrIsSuperuserOrIsStaff]
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsSuperuser]
+    permission_classes = [NotStaff]
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonListSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsStaff]
+    permission_classes = [IsStaffOrOwner]
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonDetailSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsStaff]
+    permission_classes = [IsStaffOrOwner]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsStaff]
+    permission_classes = [IsStaffOrOwner]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
-    permission_classes = [IsSuperuser]
+    permission_classes = [IsSuperuserOrIsOwner]
 
 
 class PaymentsListAPIView(generics.ListAPIView):
     serializer_class = PaymentsSerializer
     queryset = Payments.objects.all()
     filter_backends = [DjangoFilterBackend, OrderingFilter]
-    filterset_fields = ('paid_lesson', 'paid_course','payment_method_choices')
+    filterset_fields = ('paid_lesson', 'paid_course', 'payment_method_choices')
     ordering_fields = ('payment_date',)
     permission_classes = [IsAuthenticated]
 
